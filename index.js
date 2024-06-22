95abb43b2caa42a11c85786ebffad3ffe6897e09
pmNiko <nikolas090189@gmail.com>
2024-06-21 23:17:27 -0300

    Se agrego js

---
+++ index.js
@@ -0,0 +1,13 @@
+const user = {
+  name: "John Doe",
+  age: 30,
+  address: {
+    street: "123 Main St",
+    city: "New York",
+    state: "NY",
+    zip: "10001",
+  },
+  hobbies: ["reading", "painting", "cooking"],
+};
+
+console.log(user.age);
